ebf45be258ff1433d3b441bccea8f42bfb38f903
lucas240900 <132532148+lucas240900@users.noreply.github.com>
2023-05-03 23:37:38 -0300

    C:\Users\Lucas Nogueira\Downloads\web1
    envio do trabalho

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/experiencia')
+def experiencia():
+    return render_template('experiencia.html')
+
+@app.route('/formacao')
+def formacao():
+    return render_template('formacao.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
